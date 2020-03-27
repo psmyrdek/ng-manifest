83e0767562056c88caebc60c2677762ccbd6ef3d
Przemek Smyrdek <przemek.smyrdek@gmail.com>
2020-03-27 14:55:41 +0100

    Init

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # ng-manifest
-Generate JSON manifest of static files generated during Angular build
+
+Generate JSON manifest based on Angular build artifacts
+
+## Installation
+
+`npm install ng-manifest --save-dev`
+
+## Usage 
+
+`ng-manifest --root <ANGULAR_PROJECT_ROOT_DIR>`
+
+## Output
+
+```json
+{
+  "stats": {
+    "js": {
+      "legacy": [
+        {
+          "src": "runtime-es5.6a940273ca5e26c05ba0.js",
+          "type": "text"
+        },
+        {
+          "src": "polyfills-es5.aaacd79a041f45949317.js",
+          "type": "text"
+        },
+        {
+          "src": "scripts.634b7bd385f188694b28.js",
+          "type": "text"
+        },
+        {
+          "src": "vendor-es5.c259679667c7ac672a6f.js",
+          "type": "text"
+        },
+        {
+          "src": "main-es5.7776ac510f3b474d036e.js",
+          "type": "text"
+        }
+      ],
+      "modern": [
+        {
+          "src": "runtime-es2015.6a940273ca5e26c05ba0.js",
+          "type": "module"
+        },
+        {
+          "src": "polyfills-es2015.7d43a779b0c7e0afb61f.js",
+          "type": "module"
+        },
+        {
+          "src": "scripts.634b7bd385f188694b28.js",
+          "type": "text"
+        },
+        {
+          "src": "vendor-es2015.c259679667c7ac672a6f.js",
+          "type": "module"
+        },
+        {
+          "src": "main-es2015.7776ac510f3b474d036e.js",
+          "type": "module"
+        }
+      ],
+      "files": {
+        "runtimeES5": "runtime-es5.6a940273ca5e26c05ba0.js",
+        "runtimeES2015": "runtime-es2015.6a940273ca5e26c05ba0.js",
+        "polyfillsES5": "polyfills-es5.aaacd79a041f45949317.js",
+        "polyfillsES2015": "polyfills-es2015.7d43a779b0c7e0afb61f.js",
+        "vendorES5": "vendor-es5.c259679667c7ac672a6f.js",
+        "vendorES2015": "vendor-es2015.c259679667c7ac672a6f.js",
+        "mainES5": "main-es5.7776ac510f3b474d036e.js",
+        "mainES2015": "main-es2015.7776ac510f3b474d036e.js",
+        "scripts": "scripts.634b7bd385f188694b28.js"
+      }
+    },
+    "css": "styles.3d39eed1adc54183abac.css"
+  }
+}
+```
---
+++ bin/ng-manifest.js
@@ -0,0 +1,72 @@
+/* eslint-disable no-console */
+
+const glob = require('glob-promise')
+const fs = require('fs')
+const {join} = require('path')
+
+;(async () => {
+
+    const STATS_FILENAME = 'ng-manifest.json'
+
+    const projectRoot = process.argv[3]
+    if (!projectRoot) {
+        throw new Error('--root param is required!')
+    }
+
+    const options = {
+        absolute: false
+    }
+
+    const projectPath = join(process.cwd(), projectRoot)
+    const data = await glob(`${projectPath}/dist/**/+(*.js|*.css)`, options)
+
+    const fileNames = data.map(path => {
+        const parts = path.split('/')
+        return parts[parts.length - 1]
+    })
+
+    console.log('Building Angular bundles summary based on the following files:')
+    console.log(fileNames)
+
+    const bundles = {
+        stats: {
+            js: {
+                legacy: [
+                    {src: fileNames.find(f => f.match(/^runtime-es5/)), type: 'text'},
+                    {src: fileNames.find(f => f.match(/^polyfills-es5/)), type: 'text'},
+                    {src: fileNames.find(f => f.match(/^scripts/)) || '', type: 'text'},
+                    {src: fileNames.find(f => f.match(/^vendor-es5/)), type: 'text'},
+                    {src: fileNames.find(f => f.match(/^main-es5/)), type: 'text'}
+                ],
+                modern: [
+                    {src: fileNames.find(f => f.match(/^runtime-es2015/)), type: 'module'},
+                    {src: fileNames.find(f => f.match(/^polyfills-es2015/)), type: 'module'},
+                    {src: fileNames.find(f => f.match(/^scripts/)) || '', type: 'text'},
+                    {src: fileNames.find(f => f.match(/^vendor-es2015/)), type: 'module'},
+                    {src: fileNames.find(f => f.match(/^main-es2015/)), type: 'module'}
+                ],
+                files: {
+                    runtimeES5: fileNames.find(f => f.match(/^runtime-es5/)),
+                    runtimeES2015: fileNames.find(f => f.match(/^runtime-es2015/)),
+                    polyfillsES5: fileNames.find(f => f.match(/^polyfills-es5/)),
+                    polyfillsES2015: fileNames.find(f => f.match(/^polyfills-es2015/)),
+                    vendorES5: fileNames.find(f => f.match(/^vendor-es5/)),
+                    vendorES2015: fileNames.find(f => f.match(/^vendor-es2015/)),
+                    mainES5: fileNames.find(f => f.match(/^main-es5/)),
+                    mainES2015: fileNames.find(f => f.match(/^main-es2015/)),
+                    scripts: fileNames.find(f => f.match(/^scripts/)) || ''
+                }
+            },
+            css: fileNames.find(file => file.match(/^styles/))
+        }
+    }
+
+    const statsPath = join(projectPath, 'dist', STATS_FILENAME)
+    fs.writeFile(statsPath, JSON.stringify(bundles, null, 2), err => {
+        if (err) {
+            console.error(err)
+        }
+        console.log(`Successfully stored Angular bundles summary under: ${statsPath}`)
+    })
+
+})()
